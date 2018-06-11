ee3bc80c137ec6e3b650298a914333d821cbcad6
aditep <topkoka4000@gmail.com>
2018-06-12 04:57:05 +0700

    Lab_v1.1 : hamburger icon

--- app/src/main/java/com/aditep/lab_android_v1/activity/MainActivity.java
+++ app/src/main/java/com/aditep/lab_android_v1/activity/MainActivity.java
@@ -1,8 +1,12 @@
 package com.aditep.lab_android_v1.activity;
 
+import android.content.res.Configuration;
 import android.databinding.DataBindingUtil;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.aditep.lab_android_v1.R;
 import com.aditep.lab_android_v1.databinding.ActivityMainBinding;
@@ -10,16 +14,49 @@ import com.aditep.lab_android_v1.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        initInstances();
         if(savedInstanceState == null)
         {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.contentContainer, MainFragment.newInstance())
                     .commit();
         }
+    }
+
+    private void initInstances() {
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                MainActivity.this,
+                binding.drawerLayout,R.string.open_drawer,R.string.close_drawer
+        );
+        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item))
+        return true;
+        return super.onOptionsItemSelected(item);
     }
 }
